16dea8b45a93d69d14595a3c71f3e29c7b171719
jeevitha <jeevithareddy24@gmail.com>
2024-02-02 20:51:00 +0530

    payment commit

---
+++ 14-payment/variables.tf
@@ -0,0 +1,28 @@
+# default we are giving in variables
+variable "common_tags" {
+  default = {
+    Project     = "roboshop"
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+variable "tags" {
+  default = {
+    Component = "payment"
+  }
+}
+
+variable "project_name" {
+  default = "roboshop"
+}
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_name" {
+  default = "devrobo.shop"
+}
+variable "iam_instance_profile" {
+  default = "EC2RoleForShellScript"
+}
